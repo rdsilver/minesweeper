9d01ba1237e47eee3fee6d7a3126e9f3ce01794a
Reed <reedsilverstein@gmail.com>
2016-03-18 20:29:42 -0700

    gh-pages push

---
+++ jasmine/spec/board_spec.js
@@ -0,0 +1,50 @@
+'use strict';
+
+describe('Board Class Methods', () => {
+	beforeEach(() => {
+		this.testBoard = new Board(5, 2, 25, false);
+    this.otherBoard = new Board(10, 0, 25, false);
+	});
+
+	it('should create a new board with specific attributes', () => {
+		expect(this.testBoard.grid.length).toEqual(5);
+		expect(this.testBoard.grid[0].length).toEqual(5);
+    expect(this.testBoard.cellSize).toEqual(25);
+	});
+
+  it('should create a grid with 2 bombs', () => {
+    var numBombs = _.countBy(_.flatten(this.testBoard.grid), cell => cell.actual).explodedBomb;
+    expect(numBombs).toEqual(2);
+  });
+
+  it('should create a grid with 25 actual values', () => {
+    var total = 0;
+    _.each(_.countBy(_.flatten(this.testBoard.grid), cell => cell.actual), amount => total+= amount);
+    expect(total).toEqual(25);
+  });
+
+  it('should create a grid with 25 blank seen as values', () => {
+    var seenAsNum = _.countBy(_.flatten(this.testBoard.grid), cell => cell.seenAs).blank;
+    expect(seenAsNum).toEqual(25);
+  });
+
+  it('should set an attribute with setAllAttribrute method', () => {
+    this.testBoard.setAllAttribrute('test', '1');
+    var testTotal = _.countBy(_.flatten(this.testBoard.grid), cell => cell.test);
+    expect(testTotal[1]).toEqual(25);
+  });
+
+  it('checks whether to sweep the area clicked', () => {
+    this.otherBoard.checkClearArea(0, 0);
+    var numSeenAsZero = _.countBy(_.flatten(this.otherBoard.grid), cell => cell.seenAs)[0];
+
+    expect(numSeenAsZero).toEqual(100);
+  });
+
+  it('should check win condition', () => {
+    this.otherBoard.checkClearArea(0, 0);
+    this.otherBoard.checkWin();
+    expect(this.otherBoard.state).toEqual('win');
+  });
+  
+});
---
+++ solver.js
@@ -0,0 +1,148 @@
+'use strict';
+
+class Solver {
+	constructor(boardSize, numBombs, timesToSolve) {
+		this.startingTime;
+		this.boardSize = boardSize;
+		this.numBombs = numBombs;
+		this.timesToSolve = timesToSolve;
+		this.timesSolved = 0;
+	}
+
+	solve() {
+    var steps;
+		this.startingTime = millis();
+
+		while (this.timesToSolve > 0) {
+			game = new Board(this.boardSize, this.numBombs, 25, false);
+      steps = 0;
+
+      this.firstMoves();
+
+			while(game.state === 'live') {
+        var toCheck = this.checkForMustFlags();
+        this.checkForMustClicks(toCheck);
+
+        // If no progress from these methods, lets click a random blank
+        if (toCheck.length === 0) {
+          this.clickRandomBlank();
+        }
+			}
+
+			if (game.state === 'win') {
+				this.timesSolved+=1;
+			}
+
+			this.timesToSolve--;
+		}
+    var timeTaken = _.round((millis() - this.startingTime)/1000, 2);
+    console.log(('Solved: ' + this.timesSolved + ' Time Taken: ' + timeTaken + ' seconds'));
+    return [this.timesSolved, timeTaken];
+	}
+
+  firstMoves() {
+    // Click in the middle, this gives us a better chance to get a nice blank area
+    var x = _.floor(this.boardSize/2 -1);
+    var y = _.floor(this.boardSize/2 -1);
+    this.clickCell(x, y);
+
+    // We have to have an open area to work with
+    while (game.grid[x][y].seenAs !== 0 && game.state === 'live') {
+      x = _.random(0, this.boardSize-1);
+      y = _.random(0, this.boardSize-1);
+      this.clickCell(x, y);
+    }
+  }
+
+  checkForMustFlags() {
+    var toCheck = [];
+
+    _.each(game.grid, (arr, x) => {
+      _.each(arr, (cell, y)=> {
+        var seenAs = cell.seenAs;
+        var complete = cell.noMoreFlagChecking;
+
+        // We only care about cells that have a positive number
+        if (_.indexOf(['flag', 'blank', 0], seenAs) === -1 && !complete) {
+          var bombCoords = {};
+          _.each(game.bombNeighborhood, dir => {
+            var newX = x + dir[0];
+            var newY = y + dir[1];
+
+            if (notOutOfBounds(newX, newY, game.grid.length) && _.indexOf(['flag', 'blank'], game.grid[newX][newY].seenAs) >= 0) {
+              bombCoords[newX +' '+newY] =  game.grid[newX][newY].seenAs;
+            }
+          });
+
+          // If the cells bomb number is the same as the blank/flag cells around it
+          // These cells are bombs
+          if (_.keys(bombCoords).length === seenAs) {
+            _.each(_.keys(bombCoords), coords => {
+              var split = coords.split(' ');
+              var bombX = split[0];
+              var bombY = split[1];
+
+              if (bombCoords[coords] !== 'flag') {
+                game.toggleFlag(bombX, bombY);
+              }
+            });
+            cell.noMoreFlagChecking = true;
+            toCheck.push([x, y]);
+          }
+
+          // No need to flag check flags with the correct number of flags around them
+          if (_.countBy(bombCoords, _.identity).flag === seenAs) {
+            cell.noMoreFlagChecking = true;
+            toCheck.push([x, y]);
+          }
+        }
+
+      });
+    });
+
+    return toCheck;
+  }
+
+  checkForMustClicks(toCheck) {
+    _.each(toCheck, cell => {
+        var x = cell[0];
+        var y = cell[1];
+        cell = game.grid[x][y];
+
+      // If a cell's bomb number is the same as the number of flags around it
+      // We can click all the remaining blank cells knowing they arn't bombs
+        _.each(game.bombNeighborhood, dir => {
+          var newX = x + dir[0];
+          var newY = y + dir[1];
+          if (notOutOfBounds(newX, newY, game.grid.length) && game.grid[newX][newY].seenAs === 'blank') {
+            this.clickCell(newX, newY);
+          }
+        });
+    });
+  }
+
+  clickRandomBlank() {
+    var blank_coords = [];
+    var chosen;
+
+    _.each(game.grid, (arr, x) => {
+      _.each(arr, (cell, y)=> {
+        if (cell.seenAs === 'blank') {
+          blank_coords.push([x, y]);
+        }
+      });
+    });
+
+    if (blank_coords.length > 0) {
+      chosen = _.sample(blank_coords);
+      this.clickCell(chosen[0], chosen[1]);
+    }
+  }
+
+
+  // We have to press a cell before clicking it, unless I write methods in the class for just the solver
+  clickCell(x, y) {
+    game.pressCell(x, y);
+    game.clickedCell(x, y);
+  }
+}
